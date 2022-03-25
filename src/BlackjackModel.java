4aac3b59fa5a36790413bcfe2e4353052d3f31b6
Calin Pescaru <tun17456@temple.edu>
2022-03-25 02:04:09 -0400

    added basic GUI elements to view; added deck and deal functionality

--- src/BlackjackModel.java
+++ src/BlackjackModel.java
@@ -1,15 +1,113 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class BlackjackModel {
     private MainMenuModel menuModel;
     private BlackjackView blackjackView;
     private int money;
+    private LinkedList<Card> deck;
+    private Player dealer;
+    private Player player;
 
     public BlackjackModel(MainMenuModel menu, int money) {
         menuModel = menu;
         this.money = money;
+        deck = new LinkedList<>();
+        dealer = new Player();
+        player = new Player();
         blackjackView = new BlackjackView(this);
+    }
+
+    public void createDeck() {
+        deck.clear();
+        int suit; // 0 - clubs; 1 - diamonds; 2 - hearts; 3 - spades
+
+        for (int i = 0; i < 4; i++) {
+            suit = i;
+            for (int j = 1; j <= 13; j++) {
+                Card c = new Card(j, suit);
+                deck.add(c);
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+
+    public void deal() {
+        for (int i = 0; i < 4; i++) {
+            if (i % 2 == 0)
+                player.addCard();
+            else
+                dealer.addCard();
+        }
+    }
+
+    int[] popDealerCard() {
+        int[] ret = new int[2];
+        Card c = dealer.popCard();
+
+        ret[0] = c.value;
+        ret[1] = c.suit;
+
+        return ret;
+    }
+
+    int[] popPlayerCard() {
+        int[] ret = new int[2];
+        Card c = player.popCard();
+
+        ret[0] = c.value;
+        ret[1] = c.suit;
+
+        return ret;
     }
 
     public void exit() {
         menuModel.setVisible();
+    }
+
+    // DEBUG
+    public void printDeck() {
+        deck.forEach(element -> System.out.println(element.value + " " + element.suit + "\n"));
+    }
+
+    private class Card {
+        int value;
+        int suit;
+
+        Card(int v, int s) {
+            value = v;
+            suit = s;
+        }
+
+        Card(Card c) {
+            value = c.value;
+            suit = c.suit;
+        }
+    }
+
+    private class Player {
+        Card[] hand;
+        int i; // hand index
+        int j; // index for popping
+        int total;
+
+        Player() {
+            hand = new Card[10];
+            i = j = total = 0;
+        }
+
+        void addCard() {
+            Card c = new Card(deck.pollFirst());
+            hand[i++] = c;
+            total += c.value;
+        }
+
+        Card popCard() { return hand[j++]; }
+
+        int getTotal() { return total; }
     }
 }
--- src/BlackjackView.java
+++ src/BlackjackView.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,13 +10,63 @@ public class BlackjackView extends JFrame {
         super("CasinoSimulator - Blackjack");
         blackjackModel = black;
 
+        blackjackModel.createDeck();
+        blackjackModel.shuffle();
+
         setSize(300, 400);
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setResizable(false);
+        setLayout(null);
+
+        JLabel dealerLabel = new JLabel("Dealer");
+        dealerLabel.setBounds(130, 10, 50, 30);
+
+        JLabel playerLabel = new JLabel("Player");
+        playerLabel.setBounds(130, 150, 50, 30);
+
+        JButton dealBtn = new JButton("Deal");
+        dealBtn.setBounds(20, 250, 70, 30);
+
+        JButton standBtn = new JButton("Stand");
+        standBtn.setBounds(95, 250, 70, 30);
+        standBtn.setEnabled(false);
+
+        JButton hitBtn = new JButton("Hit");
+        hitBtn.setBounds(170, 250, 70, 30);
+        hitBtn.setEnabled(false);
 
         JButton exitBtn = new JButton("Exit");
-        exitBtn.setBounds(100, 100, 100, 100);
-        add(exitBtn);
+        exitBtn.setBounds(100, 330, 100, 30);
+
+        dealBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                blackjackModel.deal();
+                int[] cards;
+                JLabel cardLabel;
+                int x = 30; // x position for card labels
+
+                for (int i = 0; i < 4; i++) {
+                    if (i % 2 == 0) {
+                        cards = blackjackModel.popPlayerCard();
+                        cardLabel = new JLabel(cards[0] + " " + cards[1]);
+                        cardLabel.setBounds(x, 190, 50, 30);
+                        add(cardLabel);
+                    }
+                    else {
+                        cards = blackjackModel.popDealerCard();
+                        cardLabel = new JLabel(cards[0] + " " + cards[1]);
+                        cardLabel.setBounds(x, 50, 50, 30);
+                        add(cardLabel);
+                    }
+                    x += 30;
+                }
+                dealBtn.setEnabled(false);
+                standBtn.setEnabled(true);
+                hitBtn.setEnabled(true);
+                repaint();
+            }
+        });
 
         exitBtn.addActionListener(new ActionListener() {
             @Override
@@ -25,7 +76,13 @@ public class BlackjackView extends JFrame {
             }
         });
 
-        setLayout(null);
+        add(dealerLabel);
+        add(playerLabel);
+        add(dealBtn);
+        add(hitBtn);
+        add(standBtn);
+        add(exitBtn);
+
         setLocationRelativeTo(null);
         setVisible(true);
     }
