2b16de7ade6161343ef198bd5067738052f80d3e
Lilly Hewitt <tuo47895@temple.edu>
2024-03-18 15:15:41 -0400

    add new files and make run on intellji

---
+++ src/RouletteTest.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+
+public class RouletteTest {
+    public MainMenuModel mainMenuModel;
+    public MainMenuView mainMenuView;
+    public RouletteModel rouletteModel;
+    public RouletteView rouletteView;
+    public Robot bot;
+}
+
